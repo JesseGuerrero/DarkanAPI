4518abd62e271d4495ec33c6511b6bf341b7cdda
Jesse Guerrero <jesseguerrero1991@gmail.com>
2022-11-21 10:54:26 -0600

    My API with cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.setGlobalPrefix('v1');
+  await app.listen(8443);
 }
+
+
 bootstrap();
