f8122648c8e1c9b1355929e90b5c6a996a0a818d
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-04-05 00:12:31 -0500

    Updated daemon

--- src/temporal_hiscores/temporal_daemon.ts
+++ src/temporal_hiscores/temporal_daemon.ts
@@ -2,61 +2,71 @@ import fetch from 'node-fetch';
 import * as mongoDB from "mongodb";
 import { WORLD_MONGO_CONN_URI, WORLD_MONGO_DATABASE } from '../../config';
 
+async function updateDB() {
+    const todaysDate = new Date().toDateString()
+    try {
+        let response = await fetch(`https://darkan.org:8443/v1/highscores?page=1&limit=999999`);
+        let hiscore = await response.json();
+        let client: mongoDB.MongoClient = new mongoDB.MongoClient(WORLD_MONGO_CONN_URI);
+        await client.connect();
+        let db: mongoDB.Db = client.db(WORLD_MONGO_DATABASE);
+        let usernameHiscore = {}
+        for(let i = 0; i < hiscore.length; i++) {
+            usernameHiscore[hiscore[i].displayName] = {
+                levelDifference: hiscore[i].totalLevel,
+                xpDifference: hiscore[i].totalXp,
+                xp: hiscore[i].xp
+            }
+        }
 
-
-export function startDaemonTemporalHiscores(PORT : Number) {
-    setInterval(async () => {
-        const todaysDate = new Date().toDateString()
-        try {
-            let response = await fetch(`https://darkan.org:8443/v1/highscores?page=1&limit=999999`);
-            let hiscore = await response.json();
-            let client: mongoDB.MongoClient = new mongoDB.MongoClient(WORLD_MONGO_CONN_URI);
-            await client.connect();
-            let db: mongoDB.Db = client.db(WORLD_MONGO_DATABASE);
-            let usernameHiscore = {}
-            for(let i = 0; i < hiscore.length; i++) {
-                usernameHiscore[hiscore[i].displayName] = {
+        await db.collection("temporal").updateOne({date: todaysDate}, {$setOnInsert: {snapshot: usernameHiscore}}, { upsert: true})
+        for(let i = 0; i < hiscore.length; i++) {
+            let filter = {username: hiscore[i].displayName}
+            let player = await db.collection("temporalPlayer").findOne(filter)
+            let playerData = {}
+            if(player == null) {
+                playerData = {
+                    username: hiscore[i].displayName,
+                    dateUpdated: todaysDate,
+                }
+                playerData[todaysDate] = {
                     levelDifference: hiscore[i].totalLevel,
-                    xpDifference: hiscore[i].totalXp,
+                    totalXP: hiscore[i].totalXp,
                     xp: hiscore[i].xp
                 }
+                await db.collection("temporalPlayer").insertOne(playerData)
             }
-
-            await db.collection("temporal").updateOne({date: todaysDate}, {$setOnInsert: {snapshot: usernameHiscore}}, { upsert: true})
-            for(let i = 0; i < hiscore.length; i++) {
-                let filter = {username: hiscore[i].displayName}
-                let player = await db.collection("temporalPlayer").findOne(filter)
-                let playerData = {}
-                if(player == null) {
-                    playerData = {
-                        username: hiscore[i].displayName,
-                        dateUpdated: todaysDate,
-                    }
-                    playerData[todaysDate] = {
+            else {
+                if(player["dateUpdated"] != todaysDate) {
+                    player["dateUpdated"] = todaysDate
+                    player[todaysDate] = {
                         levelDifference: hiscore[i].totalLevel,
                         totalXP: hiscore[i].totalXp,
                         xp: hiscore[i].xp
                     }
-                    await db.collection("temporalPlayer").insertOne(playerData)
+                    playerData = player
+                    await db.collection("temporalPlayer").replaceOne(filter, playerData)
                 }
-                else {
-                    if(player["dateUpdated"] != todaysDate) {
-                        player["dateUpdated"] = todaysDate
-                        player[todaysDate] = {
-                            levelDifference: hiscore[i].totalLevel,
-                            totalXP: hiscore[i].totalXp,
-                            xp: hiscore[i].xp
-                        }
-                        playerData = player
-                        await db.collection("temporalPlayer").replaceOne(filter, playerData)
-                    }
-                }
-
             }
-            console.log("Saved hiscore at " + todaysDate)
-        } catch (e) {
-            console.log(e)
-            console.log("Error saving hiscore at " + todaysDate)
+
         }
-    }, 1_000*60*60*12)//every 12 hours
+        console.log("Saved hiscore at " + todaysDate)
+    } catch (e) {
+        console.log(e)
+        console.log("Error saving hiscore at " + todaysDate)
+    }
+}
+
+export function startDaemonTemporalHiscores(PORT : Number) {
+    function runAtMidnight() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+
+        if (hours === 0 && minutes === 0) {
+            updateDB()
+        }
+    }
+    updateDB().then(r => setInterval(runAtMidnight, 1_000*60))
+
 }
